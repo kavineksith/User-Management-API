afa4a114f7f8e5551dff96cf548b09e6c6860370
Kavin Eksith <143245901+kavineksith@users.noreply.github.com>
2024-06-06 21:59:14 +0530

    Update validateInputs.js

--- middleware/validateInputs.js
+++ middleware/validateInputs.js
@@ -1,12 +1,24 @@
-const { validationResult } = require('express-validator');
+const { validationResult, check } = require('express-validator');
 
-// Middleware for input validation
-const validateInput = (req, res, next) => {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-        return res.status(400).json({ errors: errors.array() });
+// Middleware to handle input validation
+const validateInput = [
+    check('id').notEmpty().withMessage('ID is required'),
+    check('fname').notEmpty().withMessage('First name is required'),
+    check('lname').notEmpty().withMessage('Last name is required'),
+    check('dateOfBirth').notEmpty().withMessage('Date of birth is required'),
+    check('address').notEmpty().withMessage('Address is required'),
+    check('emailAddress').notEmpty().withMessage('Email address is required').isEmail().withMessage('Invalid email address'),
+    check('contactNumber').notEmpty().withMessage('Contact number is required'),
+    check('country').notEmpty().withMessage('Country is required'),
+    (req, res, next) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return next({ status: 400, errors: errors.array() }); // Pass errors to the next middleware or route handler
+        }
+        next(); // Proceed to the next middleware or route handler if validation passes
     }
-    next();
-};
+];
 
-module.exports = { validateInput };
+module.exports = {
+    validateInput
+};
